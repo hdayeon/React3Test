bef72b81e9f0d71c443ed0b960213d84b4338171
hdayeon <gkekdus97@naver.com>
2023-11-16 11:08:23 +0900

    주특기 3주차 테스트(React) “느린” 투두 리스트

---
+++ src/redux/modules/todosSlice.js
@@ -0,0 +1,48 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+/**
+ * 2초 지연을 시키는 함수입니다 (비동기 작업).
+ */
+import { waitTwoSeconds } from '../../utils';
+
+export const __addToDo = createAsyncThunk(
+  '__addToDo',
+  async (payload, thunkAPI) => {
+    await waitTwoSeconds();
+    thunkAPI.dispatch(addTodo(payload));
+    console.log(payload); // payload 확인용
+  }
+);
+
+export const __deleteTodo = createAsyncThunk(
+  '__deleteToDo',
+  async (payload, thunkAPI) => {
+    await waitTwoSeconds();
+    thunkAPI.dispatch(deleteTodo(payload));
+    console.log(payload); // payload 확인용
+  }
+);
+
+const initialState = {
+  list: [],
+};
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      return {
+        list: [...state.list, action.payload]
+      };
+    },
+    deleteTodo: (state, action) => {
+      return {
+        list: state.list.filter((item) => item.id !== action.payload)
+      };
+    },
+  },
+});
+
+export const { addTodo, deleteTodo } = todosSlice.actions;
+export default todosSlice.reducer;
